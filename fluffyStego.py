ccccd7d6421b4d8aaee41dc4753b0b1bc75f3eba
William <william.laycraft@gmail.com>
2016-03-24 20:39:32 -0700

    big update to hide covert img

--- fluffyStego.py
+++ fluffyStego.py
@@ -4,19 +4,44 @@ from PIL import Image, ImageMath
 
 class fluffyStego:
 	def hideIMG(sourceIMG, covertIMG, passphrase):
-		rgbSource	= sourceIMG.convert("RGB")
 		rgbCovert	= covertIMG.convert("RGB")
+						
+		rBitArray = ""
+		gBitArray = ""
+		bBitArray = ""
+ 		
+		covertPixels = list(rgbCovert.getdata())
+
+		for p in covertPixels:
+			pR, pG, pB = p	
+	
+			singleR = str(bin(pR)[+2:]).zfill(8)
+			for x in singleR.split():
+				rBitArray += x
+
+			singleG = str(bin(pG)[+2:]).zfill(8)
+			for x in singleG.split():
+				gBitArray += x
+
+			singleB = str(bin(pB)[+2:]).zfill(8)
+			for x in singleB.split():
+				bBitArray += x
 
 		tX, tY, bX, bY 	= rgbSource.getbbox()
-		stegoImg	= Image.new("RGB", (bX,bY))
+		stegoIMG		= Image.new("RGB", (bX,bY))
+		pixelIndex 		= 0
+		rgbSource	= sourceIMG.convert("RGB")
 
 		for x_loc in range(0,int(bX)):
 			for y_loc in range(0,int(bY)):
-				r, g, b = rgb_image.getpixel((x_loc,y_loc))
-				r = int(bin(r|1) , 2)
-				g = int(bin(g|1) , 2)
-				b = int(bin(b|1) , 2)	
-				out.putpixel((x_loc,y_loc),(r,g,b))
+				r, g, b = rgb_Source.getpixel((x_loc,y_loc))
+				r = int(bin(r|int(rBitArray[pixelIndex])) , 2)
+				g = int(bin(g|int(gBitArray[pixelIndex])) , 2)
+				b = int(bin(b|int(bBitArray[pixelIndex])) , 2)					
+														
+				stegoIMG.putpixel((x_loc,y_loc),(r,g,b))
+
+				pixelIndex+1
 		
 		return stegoIMG 
 
