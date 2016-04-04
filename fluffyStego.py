c2090f1ebe93edfeb8de1c68423d40b30b8ee44f
wetspidermonkey <wetspidermonkey@gmail.com>
2016-04-03 18:55:08 -0700

    sunday, code inject confirmed

--- fluffyStego.py
+++ fluffyStego.py
@@ -6,7 +6,7 @@ import re
 def hideIMG(sourceIMG, covertIMG, passphrase):
 	rgbCovert	= covertIMG.convert("RGB")
 	rgbSource	= sourceIMG.convert("RGB")
-					
+				
 	rBitArray = ""
 	gBitArray = ""
 	bBitArray = ""
@@ -30,17 +30,38 @@ def hideIMG(sourceIMG, covertIMG, passphrase):
 
 	tX, tY, bX, bY 	= rgbSource.getbbox()
 	stegoIMG		= Image.new("RGB", (bX,bY))
-	
+
 	pixelIndex 		= 0
 	
 	for x_loc in range(0,int(bX)):
 		for y_loc in range(0,int(bY)):
 			r, g, b = rgbSource.getpixel((x_loc,y_loc))
 			if pixelIndex < len(rBitArray)/8:
-				rC = int(bin(r|int(rBitArray[pixelIndex])) , 2)
-				gC = int(bin(g|int(gBitArray[pixelIndex])) , 2)
-				bC = int(bin(b|int(bBitArray[pixelIndex])) , 2)
-				stegoIMG.putpixel((x_loc,y_loc),(rC,gC,bC))
+				if r%2==0: 
+					if int(rBitArray[pixelIndex])==1:
+						r+=1
+				else:
+					if int(rBitArray[pixelIndex])==0:
+						r-=1
+
+				if g%2==0: 
+					if int(gBitArray[pixelIndex])==1:
+						g+=1
+				else:
+					if int(gBitArray[pixelIndex])==0:
+						g-=1
+
+				if b%2==0: 
+					if int(bBitArray[pixelIndex])==1:
+						b+=1
+				else:
+					if int(bBitArray[pixelIndex])==0:
+						b-=1
+
+				#rC = int(bin(r|int(rBitArray[pixelIndex])) , 2)
+				#gC = int(bin(g|int(gBitArray[pixelIndex])) , 2)
+				#bC = int(bin(b|int(bBitArray[pixelIndex])) , 2)
+				stegoIMG.putpixel((x_loc,y_loc),(r,g,b))
 				pixelIndex += 1				
 			else:								
 				stegoIMG.putpixel((x_loc,y_loc),(r,g,b))
@@ -55,40 +76,62 @@ def findImg(rgbSource, passphrase):
 	gBitArray = ""
 	bBitArray = ""
 
-	#foundIMG		= Image.new("RGB", (bX,bY))
-	foundIMG		= Image.new("RGB", (128,128))
+	covertPixels = list(rgbSource.getdata())
 
-	for x_loc in range(0,int(bX)):
-		for y_loc in range(0,int(bY)):
-			r, g, b = rgbSource.getpixel((x_loc,y_loc))
-			rBitArray += str(bin(r)[+9:])
-			gBitArray += str(bin(g)[+9:])
-			bBitArray += str(bin(b)[+9:])	
-															
+	for p in covertPixels:
+		pR, pG, pB = p	
+
+		singleR = str(bin(pR)[+2:])
+		for x in singleR.split():
+			rBitArray += x[-1:]
+
+		singleG = str(bin(pG)[+2:])
+		for x in singleG.split():
+			gBitArray += x[-1:]
+
+		singleB = str(bin(pB)[+2:])
+		for x in singleB.split():
+			bBitArray += x[-1:]
+
+	#for x_loc in range(0,int(bX)):
+		#for y_loc in range(0,int(bY)):
+			#r, g, b = rgbSource.getpixel((x_loc,y_loc))
+			#print str(bin(r)[-1:])
+			#rBitArray += str(bin(r)[-1:])
+			#gBitArray += str(bin(g)[-1:])
+			#bBitArray += str(bin(b)[-1:])
+													
 	rCovert = re.findall('........', rBitArray)
 	gCovert = re.findall('........', gBitArray)
 	bCovert = re.findall('........', bBitArray)
-	print len(bCovert)
+	print rBitArray[:128] 	
+	#print rCovert, gCovert, bCovert	
+	#print rCovert[1], gCovert[1], bCovert[1]
+
+	#foundIMG		= Image.new("RGB", (bX,bY))
+	foundIMG		= Image.new("RGB", (128,128))
 
 	pixelIndex = 0
 
-	for x_loc in range(0,128):
-		for y_loc in range(0,128):
-			if pixelIndex < len(rCovert):		
-				rC = int(rCovert[pixelIndex])
-				gC = int(gCovert[pixelIndex])
-				bC = int(bCovert[pixelIndex])
+	for x_loc in range(0,127):
+		for y_loc in range(0,127):
+		#if pixelIndex < len(rCovert):		
+			rC = int(rCovert[pixelIndex], 2)
+			gC = int(gCovert[pixelIndex], 2)
+			bC = int(bCovert[pixelIndex], 2)
 
-				foundIMG.putpixel((x_loc,y_loc),(rC,gC,bC))
-				#print foundIMG.getpixel((x_loc,y_loc))
-				#print rC,gC,bC
-				#print x_loc,y_loc
-				pixelIndex += 1
+			#print rC, gC, bC
+
+			foundIMG.putpixel((x_loc,y_loc),(rC,gC,bC))
+			#print foundIMG.getpixel((x_loc,y_loc))
+			#print rC
+			#print x_loc,y_loc
+			pixelIndex += 1
 	
 	return foundIMG
 
 def checkSizeOK(sourceIMG, covertIMG):
-	sourcePixelTotal = sourceIMG.width() * sourceIMG.height()
-	covertPixelTotal = sourceIMG.width() * sourceIMG.height()
+	sourcePixelTotal = sourceIMG.width * sourceIMG.height
+	covertPixelTotal = sourceIMG.width * sourceIMG.height
 	print sourcePixelTotal + covertPixelTotal
-
+	return True
